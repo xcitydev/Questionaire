59681bfa99cced3ec5e5914c6f531bd8c67fca29
Kingsley Chuwkuemeka <64647601+ChukwuemekaKingsley@users.noreply.github.com>
2022-06-26 23:09:18 +0100

    make it better

--- app.js
+++ app.js
@@ -100,3 +100,6 @@ app.delete("/:id", async (req, res) => {
     res.status(500).send(error);
   }
 });
+server.listen(process.env.PORT || 5000, () => {
+  console.log("Listening on port 3001");
+});
